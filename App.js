80b05f8dba1411c31f05050b115bae2085f11d35
Matthew Denune <mattdenune@gmail.com>
2024-04-09 07:32:26 -0400

    changed Drawer to BottomTab

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import {createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -8,29 +9,23 @@ import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
 
 const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName='User' screenOptions={{
-        drawerActiveBackgroundColor: '#72dc54',
-        drawerActiveTintColor: '#2343f6',
-        drawerStyle: {backgroundColor: '#3cebfa'}
-      }}>
-        <Drawer.Screen name='Welcome' component={WelcomeScreen} options={{
-          headerStyle: {backgroundColor: '#3c0a6b'},
-          headerTintColor: 'white',
-          drawerLabel: 'Welcome Screen',
-          drawerIcon: ({color, size}) => (
-            <Ionicons name='home'color={color} size={size} />
-          )
-        }} />
-        <Drawer.Screen name='User' component={UserScreen}  options={{
-          drawerIcon: ({color, size}) => (
-            <Ionicons name='person'color={color} size={size} />
+      <BottomTab.Navigator initialRouteName='User'>
+        <BottomTab.Screen name='Welcome' component={WelcomeScreen} options={{
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name='home' color={color} size={size} />
           )
         }}/>
-      </Drawer.Navigator>
+        <BottomTab.Screen name='User' component={UserScreen}  options={{
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name='person' color={color} size={size} />
+          )
+        }}/>
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }
