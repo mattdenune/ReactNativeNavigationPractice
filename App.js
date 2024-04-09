c64f0a06d4d95d8240b4b8c5a8db44ee72ccddee
Matthew Denune <mattdenune@gmail.com>
2024-04-09 07:12:18 -0400

    added a button to toggle drawer in UserScreen component

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import {createDrawerNavigator } from '@react-navigation/drawer';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
@@ -20,8 +21,15 @@ export default function App() {
           headerStyle: {backgroundColor: '#3c0a6b'},
           headerTintColor: 'white',
           drawerLabel: 'Welcome Screen',
+          drawerIcon: ({color, size}) => (
+            <Ionicons name='home'color={color} size={size} />
+          )
         }} />
-        <Drawer.Screen name='User' component={UserScreen} />
+        <Drawer.Screen name='User' component={UserScreen}  options={{
+          drawerIcon: ({color, size}) => (
+            <Ionicons name='person'color={color} size={size} />
+          )
+        }}/>
       </Drawer.Navigator>
     </NavigationContainer>
   );
